ce37da3595a2159595c681a8fff0dc38e15023f8
stops0 <see2003a@naver.com>
2019-03-30 20:21:23 +0900

    first commit

--- app/src/main/java/com/example/c401_47_pc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/c401_47_pc/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
